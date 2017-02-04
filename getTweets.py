698d49afc69cdb9ea0c1a9ddcf718a6f687cb2ff
Brian Norman <brian@Brians-MacBook-Pro.local>
2017-02-04 15:33:12 -0500

    Fix

--- getTweets.py
+++ getTweets.py
@@ -4,33 +4,32 @@ import json
 
 from api import getAPI
 
-REQUEST_DELAY = 5 # Helps us avoid rate limits of Twitter API
-MAX_REQUESTS = 5
+REQUEST_DELAY = 5
+MAX_REQUESTS = 15
 
 def main():
-	try:
-		arg = argv[1]
-		api = getAPI()
-		tweetResults = []
+    try:
+        arg = argv[1]
+        api = getAPI()
+        tweetResults = []
 
-		tweetIndex = api.user_timeline(screen_name=arg, count=1)[0].id # Gives ID of the most recent tweet
-		time.sleep(REQUEST_DELAY)
-		for request in range(MAX_REQUESTS):
-			tweets = api.user_timeline(screen_name=arg, include_retweets=False, max_id=tweetIndex)
-			for tweet in tweets:
-				tweetResults.append(tweet.text)
-				tweenIndex = tweet.id
-			time.sleep(REQUEST_DELAY)
+        tweetIndex = api.user_timeline(screen_name=arg, count=1)[0].id
+        time.sleep(REQUEST_DELAY)
+        for request in range(MAX_REQUESTS):
+            tweets = api.user_timeline(screen_name=arg, include_retweets=False, max_id=tweetIndex)
+            for tweet in tweets:
+                tweetResults.append(tweet.text)
+                tweetIndex = tweet.id
+            time.sleep(REQUEST_DELAY)
 
-	except IndexError:
-		print("Program Missing Arg. Twitter Handle")
-	except Exception as e:
-		print("Program Failure. Error: {}".format(e))
-	finally:
-		with open('{}Tweets'.format(arg), 'w') as saveFile:
-			json.dump(tweetResults, saveFile)
+    except IndexError:
+        print("Program Missing Arg. Twitter Handle")
+    except Exception as e:
+        print("Program Failure.  Error: {}".format(e))
+    finally:
+        with open("{}Tweets".format(arg), 'w') as saveFile:
+            json.dump(tweetResults, saveFile)
+            print("File Saved.")
 
 if __name__ == '__main__':
-	main()
-
-# Usage: python getTweets.py [screen name]
+    main()
