afc7bbc3a5324282ecd3f6ca2fd56a52a933dd7d
Brian Norman <brian@Brians-MacBook-Pro.local>
2017-03-30 07:08:27 -0400

    Added comment

--- getTweets.py
+++ getTweets.py
@@ -5,10 +5,12 @@ import json
 from api import getAPI
 
 REQUEST_DELAY = 5
-MAX_REQUESTS = 1
+MAX_REQUESTS = 60 # multiply this number by 20 and thats the number of tweets in the past
 
 def main():
     try:
+        count = 0
+
         #arg = argv[1]
         arg = "realDonaldTrump"
         api = getAPI()
@@ -19,10 +21,11 @@ def main():
         for request in range(MAX_REQUESTS):
             tweets = api.user_timeline(screen_name=arg, include_retweets=False, max_id=tweetIndex)
             for tweet in tweets:
+                count += 1
                 tweetResults.append(tweet.text)
                 tweetIndex = tweet.id
             time.sleep(REQUEST_DELAY)
-
+        print("Number of tweets {}".format(str(count)))
     except IndexError:
         print("Program Missing Arg. Twitter Handle")
     except Exception as e:
