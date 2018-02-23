229add948f1d5a6712ef01fcb1114f48bfd166be
Sunny Nagam <sunnynagam1@gmail.com>
2018-02-23 15:31:10 -0700

    Fixed bug where tweets json was being analyzed before it tweets were retrived and file was created.

--- tweetGenerator.py
+++ tweetGenerator.py
@@ -5,11 +5,11 @@ import getTweets
 
 tweet = "Trump's most used words:\n"
 
-for i in range(len(list_of_words)):
-	tweet += "{}: {} \n".format(list_of_words[i][0], list_of_words[i][1])
-
 print("Updating log")
 getTweets.main()
+
+for i in range(len(list_of_words)):
+	tweet += "{}: {} \n".format(list_of_words[i][0], list_of_words[i][1])
 
 api = getAPI()
 api.update_status(tweet)
